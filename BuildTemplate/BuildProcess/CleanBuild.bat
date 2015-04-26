aab2a56325b9199f7bc3fb0af25dc0f5f235b7b7
Shawn Mclean <shawnrmclean@gmail.com>
2015-04-26 05:48:18 -0500

    updated clean file to use rimraf

--- BuildTemplate/BuildProcess/CleanBuild.bat
+++ BuildTemplate/BuildProcess/CleanBuild.bat
@@ -1,5 +1,7 @@
 @echo off
 rem Clean
 set folder="%1"
-cd /d "%folder%"
-for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+CMD /C npm install -g rimraf
+echo "cleaning %folder%"
+CMD /C rimraf %folder%
+echo "rimraf completed"
